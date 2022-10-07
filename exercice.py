cc48e7e7baaa53a45821e69bb8dcceb5f44e21c4
raphaeldub3 <raphael.dube@polymtl.ca>
2022-10-07 17:15:02 -0400

    Pas fini

--- exercice.py
+++ exercice.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+import math
 
 
 # TODO: Définissez vos fonction ici
+def masse_volume(a=1, b=2, c=3, m=4):
+    V = 4/3 * math.pi * a * b * c
+    mV = V * m
+    return V, mV
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-    pass
+    result = masse_volume(c=5, m=10)
+    print(result)
