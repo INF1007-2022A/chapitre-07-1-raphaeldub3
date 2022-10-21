643db777b30e7840ca628a2907a0c93323c3d780
raphaeldub3 <raphael.dube@polymtl.ca>
2022-10-21 17:34:28 -0400

    Ex_02 fait

--- exercice.py
+++ exercice.py
@@ -6,13 +6,22 @@ import math
 
 
 # TODO: Définissez vos fonction ici
-def masse_volume(a=1, b=2, c=3, m=4):
-    V = 4/3 * math.pi * a * b * c
-    mV = V * m
-    return V, mV
+def ex_01(a=1, b=2, c=3, m=4):
+    v = 4/3 * (math.pi * a * b * c)
+    mv = v * m
+    return v, mv
+
+def ex_02(sentence):
+    keys = list(set(sentence))
+    dic = {key: sentence.count(key) for key in keys if sentence.count(key) > 5}
+    dic = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+    return dic
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-    result = masse_volume(c=5, m=10)
+    result = ex_01(c=5, m=10)
     print(result)
+
+    sentence = "The fuck d'you say to me you little shit"
+    print(ex_02(sentence))
